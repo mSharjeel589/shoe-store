5c207f664aef9fa3b5f1cfb426d7f182b5e090aa
mSharjeel589 <msharjeel589@gmail.com>
2025-04-26 23:25:50 +0500

    Header Completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <section className="max-w-screen-[1920px]">
+      <img src="./src/assets/shoeBG.png" alt="" className="absolute right-0
+      2xl:h-[100%]
+      " />
+
+      <div className="flex flex-row items-center justify-between gap-4">
+        <img src="./src/assets/siteLogo.png" alt="LOGO" className="ml-8 
+        [@media(min-width:1920px)]:w-52 [@media(min-width:1920px)]:h-48
+        2xl:w-42 2xl: h-38
+        " />
+        <nav className="space-x-12 mr-8 text-white text-3xl">
+          <a href="#hero">Home</a>
+          <a href="#">How it works?</a>
+          <a href="#">About</a>
+          <a href="#">Testimonials</a>
+          <a href="#">Contact Us</a>
+        </nav>
+      </div>
+
+      <div id="hero" className="relative">
+        <div className="flex flex-row items-center justify-center 
+        [@media(min-width:1920px)]:gap-60
+        2xl: gap-32">
+          <p className="
+          [@media(min-width:1920px)]:text-9xl
+          2xl: text-8xl
+           font-bold text-white leading-none">
+            THE <br />
+            SHOE <br />
+            COMPANY
+          </p>
+          <img src="./src/assets/heroShoe.png" alt="SHOE" className="2xl: w-[40%]" />
+        </div>
+      </div>
+    </section>
+  );
+}
